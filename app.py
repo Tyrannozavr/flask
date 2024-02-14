44218c37ff1203f8a7ff612e83c5168c8f89c6c7
dmiv <Dmitriy40647274@gmail.com>
2024-02-14 18:41:24 +0300

    обновление данных пользователя

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
 
 if __name__ == 'main':
     app.run(debug=True)
@@ -16,22 +9,53 @@ if __name__ == 'main':
 # подключение базы данных
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(app)
+
+
 class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(80), unique=True, nullable=False)
-  balance = db.Column(db.Integer, unique=False, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    balance = db.Column(db.Integer, unique=False, nullable=False)
 
-  def __init__(self, username, balance):
-      self.username = username
-      self.balance = balance
-      super().__init__()
+    def __init__(self, username, balance):
+        self.username = username
+        self.balance = balance
+        super().__init__()
 
-  def __repr__(self):
+    def add_user(self, username):
+        pass
+
+    def update_user(self, username=None, balance=None):
+        self.username = username
+        self.balance = balance
+        db.session.commit()
+        return self
+
+    def __repr__(self):
         return f'<User {self.username}>'
+
+
+# добавления, обновления и удаления пользователей и обновления их балансов
+from flask import request
+
+@app.route('/update/<id>')
+def update(id):
+    username = request.args.get('username')
+    balance = request.args.get('balance')
+    user = User.query.filter_by(id=id).first()
+    if user is not None:
+        user.username = username
+        user.balance = balance
+        db.session.commit()
+    return jsonify({'name': user.username, 'user': user.balance})
+
 
 with app.app_context():
     db.create_all()
-    user = User('John', 2000)
-    db.session.add(user)
-    db.session.commit()
-
+    names = ['John', 'Smith', 'Mike', 'David', 'Jack']
+    balances = [5000, 6000, 7000, 8000, 10000]
+    query = User.query.all()
+    if len(query) == 0:
+        for name, price in zip(names, balances):
+            user = User(name, price)
+            db.session.add(user)
+        db.session.commit()
