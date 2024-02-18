f7fade4562fb290ca81e7670f8830d6694ea4167
dmiv <Dmitriy40647274@gmail.com>
2024-02-18 13:21:25 +0300

    asyncio updated

--- utils.py
+++ utils.py
@@ -14,29 +14,41 @@ def request_temperature(city: str) -> float:
     request = requests.get(url=f'http://api.weatherapi.com/v1/current.json?key={my_key}&q={city}')
     temperature = request.json().get('current').get('temp_c')
     return float(temperature)
-async def task_coroutine():
-    # вывод сообщения
-    print('executing the task')
-    # блокировка на некоторое время
-    await asyncio.sleep(1)
+
+
 async def refresh_data(city: str):
     temperature = request_temperature(city)
     weather = Weather.get_weather(city)
-    weather.temperature = temperature
-    weather.datetime = datetime.datetime.now()
-    # db.session.add()
+    # print('weather in refresh is ', weather)
+    if weather is None:
+        weather = Weather(city, temperature, datetime.datetime.now())
+        print(weather, 'now')
+        db.session.add(weather)
+    else:
+        weather.temperature = temperature
+        weather.datetime = datetime.datetime.now()
     db.session.commit()
 
 async def fetch_weather(city: str) -> float:
     weather = Weather.get_weather(city)
+    if weather is None:
+        print('is none')
+        await asyncio.create_task(refresh_data(city))
+        print('await')
+        weather = Weather.get_weather(city)
+        print(weather)
+    print('after if')
     weather_datetime = weather.datetime
+    print('after weather datetime')
     different = datetime.datetime.now() - weather_datetime
-    # if different.total_seconds() > 300:
-    if True:
-        print('run')
+    if different.total_seconds() > 6000:
+        await refresh_data(city)
+        weather = Weather.get_weather(city)
+        print('fetch data')
+    elif different.total_seconds() > 3000:
         asyncio.create_task(refresh_data(city))
-        # task = asyncio.create_task(task_coroutine())
-        print('end')
+        print('refresh data')
+    print('end')
     return weather
 
 
